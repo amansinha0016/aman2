b49845111697a77f019a25712f13ed4b88fbdcc3
amansinha0016 <amansinha0016.com>
2024-04-26 18:34:35 +0530

    first_commit

---
+++ site-visit.blade.php
@@ -0,0 +1,367 @@
+<x-user.header-without-chart>
+    
+  <div class="app-content">
+    <div class="content-wrapper">
+      <div class="row mt-4">
+        <div class="col">
+          <div class="card">
+            <div class="card-header d-flex justify-content-between">
+              <h5 class="card-title">Site Visitor</h5>
+<a href="{{route('user.home.SiteVisit.SiteVisitStages')}}" style="background:#00ffdc;">Site Visit Stages</a>
+<a href="{{route('user.home.AddSiteVisit.Addnew')}}" style="background:#00ffdc;">Add Site Visit</a>
+            </div>
+<div class="container" style="margin-left:30px;">
+
+             <div class="row bg-white filter-container">
+                <div class="col-lg-6 text-end">
+                      <div class="offcanvas offcanvas-end text-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                        <div class="offcanvas-header bb-border">
+                          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">All filter</h5>
+                          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div class="offcanvas-body">
+                            <div class="accordion accordion-flush" id="accordionFlushExample">
+                                <div class="accordion-item">
+                                  <h2 class="accordion-header">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                      Preferred Destination
+                                    </button>
+                                  </h2>
+                                  <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <label class="labels">
+                                                Countries
+                                            </label>
+                                            <select class="e3 form-control select-2-input w-100" multiple="multiple">
+                                                <option>Canada</option>
+                                              </select>
+                                        </div>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div class="accordion-item">
+                                  <h2 class="accordion-header">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                                      Study Level
+                                    </button>
+                                  </h2>
+                                  <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                                    <p>
+                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere obcaecati dignissimos, sunt quos neque eius consequatur. Earum illum ex necessitatibus harum? Magnam ea corporis minus quos doloribus, exercitationem molestiae ipsa?
+                                    </p>
+                                  </div>
+                                </div>
+                                <div class="accordion-item">
+                                  <h2 class="accordion-header">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                                      Field of Study
+                                    </button>
+                                  </h2>
+                                  <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                                    <p>
+                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere obcaecati dignissimos, sunt quos neque eius consequatur. Earum illum ex necessitatibus harum? Magnam ea corporis minus quos doloribus, exercitationem molestiae ipsa?
+                                    </p>
+                                  </div>
+                                </div>
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header">
+                                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse4" aria-expanded="false" aria-controls="flush-collapse4">
+                                        Intake Year
+                                      </button>
+                                    </h2>
+                                    <div id="flush-collapse4" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                                        <p>
+                                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere obcaecati dignissimos, sunt quos neque eius consequatur. Earum illum ex necessitatibus harum? Magnam ea corporis minus quos doloribus, exercitationem molestiae ipsa?
+                                        </p>
+                                    </div>
+                                  </div>
+                                  <div class="accordion-item">
+                                    <h2 class="accordion-header">
+                                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse5" aria-expanded="false" aria-controls="flush-collapse5">
+                                        Mode of Study
+                                      </button>
+                                    </h2>
+                                    <div id="flush-collapse5" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                                        <p>
+                                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere obcaecati dignissimos, sunt quos neque eius consequatur. Earum illum ex necessitatibus harum? Magnam ea corporis minus quos doloribus, exercitationem molestiae ipsa?
+                                        </p>
+                                    </div>
+                                  </div>
+                                  <div class="accordion-item">
+                                    <h2 class="accordion-header">
+                                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse6" aria-expanded="false" aria-controls="flush-collapse6">
+                                        Duration
+                                      </button>
+                                    </h2>
+                                    <div id="flush-collapse6" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                                        <p>
+                                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere obcaecati dignissimos, sunt quos neque eius consequatur. Earum illum ex necessitatibus harum? Magnam ea corporis minus quos doloribus, exercitationem molestiae ipsa?
+                                        </p>
+                                    </div>
+                                  </div>
+                              </div>
+                              <div class="mt-3">
+                                <input type="checkbox"> Top Placement
+                              </div>
+                              <div class="col-12 text-center py-3 filter-bbt">
+                                <a class="btn-course d-inline-block">
+                                    Find Courses
+                                    <img src="https://99asset.com/wp-content/uploads/2023/01/99-ASSET-BLUE-WHITE-LOGO.png" class="img-fluid">
+                                </a>
+                            </div>
+                        </div>
+                      </div>
+            
+                </div>
+            </div>   
+    
+<div class="row">
+        
+  <div class="col-6">
+       <div class="row" style="border:1px solid #4b4b4b;">
+     <div class="col-1" style="font-size:30px;padding:5px;">
+      <i class="fa fa-search" aria-hidden="true"></i>
+     </div>
+     <div class="col-11" style="padding:0;">        
+   <input type="search" onkeyup="SearchOnKeyUpp(this)" style="border:none;box-shadow:none !important;" class="form-control" id="exampleInputEmail1" aria-describedby="search">
+     </div>
+  </div>
+  </div>
+
+    </div>
+</div>            
+            <div class="card-body" style="overflow-x:scroll;" id="sitevisitsethtmltothisid">
+                <span>Total:{{$SiteVisit->total()}}</span>
+              <table class="table table-hover overflow-hidden fs--1" id="mytable">
+                <thead class="bg-200 text-dark">
+                  <tr style="background:#4b4b4b;color:#fff;">
+                    <th class="headcol colcheck"><input type="checkbox" id="checkall" value="1"></th>  
+    <th style="font-weight:700; width:5%">SL No.</th>
+    <th style="font-weight:700;" class="name">Client Name</th>
+      <th style="font-weight:700;">Action</th>
+    <th style="font-weight:700;" class="phone">Phone </th>
+    <th style="font-weight:700;" class="property">Property ID</th>
+    <th style="font-weight:700;" class="property_type">Property Name </th>
+        <th style="font-weight:700;" class="deal_stage">Site Visit Stage </th>
+<th style="font-weight:700;" class="deal_stage">Current Stage Status</th>        
+    <th style="font-weight:700;" class="followup_time">Followup Time </th>
+      
+                  </tr>
+                </thead>
+@foreach($SiteVisit as $incrrkey=>$SiteVisits)           
+                <tbody>
+                  <tr class="align-middle append_row" id="">
+                     <td class="text-nowrap"><input type="checkbox" id="checkall" value="1"></td> 
+                    <td class="text-nowrap">{{$incrrkey+1}}</td>
+                    <td class="text-nowrap">{{$SiteVisits->clients->client_name}}</td>
+                    
+                    <td class="text-nowrap"><a href="{{route('user.home.SiteVisit.SiteVisitDelete')}}?sitevisit_ctl_id={{$SiteVisits->id}}"><i class="fa fa-trash" aria-hidden="true"></i></a>
+                    @if($incrrkey==0 && $SiteVisits->site_visit_stage!='Completed')
+                     @if($SiteVisits->stage_completed==1)
+                    <button onclick="SetValueToModal(`{{$SiteVisits->id}}`,`{{$SiteVisits->site_visit_stage}}`,`{{$SiteVisits->follow_up_time}}`,`{{$SiteVisits->user_id}}`)" style="margin-left:20px;" data-toggle="modal" data-target="#newEventModal22"  class="btn btn-info" aria-hidden="true">Set New FollowUp Date</button>
+                    @endif
+               <button onclick="SetValueToModal11(`{{$SiteVisits->id}}`,`{{$SiteVisits->site_visit_stage}}`,`{{$SiteVisits->follow_up_time}}`,`{{$SiteVisits->user_id}}`)" style="margin-left:20px;" data-toggle="modal" data-target="#newEventModal11" aria-hidden="true" class="btn btn-info">Update Current Stage Status</button>
+               @endif
+<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 192 512" class="fa-solid fa-list-dropdown dropdown-toggle" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"><path d="M96 184c39.8 0 72 32.2 72 72s-32.2 72-72 72-72-32.2-72-72 32.2-72 72-72zM24 80c0 39.8 32.2 72 72 72s72-32.2 72-72S135.8 8 96 8 24 40.2 24 80zm0 352c0 39.8 32.2 72 72 72s72-32.2 72-72-32.2-72-72-72-72 32.2-72 72z"/></svg>               
+    <!--<i style="margin-left:10px;" class="fa-solid fa-list-dropdown dropdown-toggle" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"></i>-->
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <li><a class=" btn-filter dropdown-item"  type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation" href="#">Action</a></li>
+    <li><a class=" btn-filter dropdown-item"  type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation" href="#">Another action</a></li>
+    <li><a class=" btn-filter dropdown-item"  type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation" href="#">Something else here</a></li>
+  </ul>             
+                    </td>                    
+                    <td class="text-nowrap">{{$SiteVisits->phone}}</td>
+                    <td class="text-nowrap">{{$SiteVisits->property_id}}</td>                    
+                    <td class="text-nowrap">{{$SiteVisits->properties->property_name}}</td>
+                    <td class="text-nowrap"><b style="color:{{$SiteVisits->sitevisitstages->stage_color}}">{{$SiteVisits->site_visit_stage}}</b></td>    
+                     <td class="text-nowrap">
+                    @if($SiteVisits->stage_completed==1)
+                    <b style="color:green;">Completed</b>
+                    @else
+                    @if($SiteVisits->stage_completed==2)
+                    <b style="color:pink;">Hold</b>  
+                    @else          
+                    <b style="color:red;">Pending</b>
+                    @endif 
+                    @endif
+                     </td>
+                    
+                    <td class="text-nowrap">{{$SiteVisits->follow_up_time}}</td>
+
+                  </tr>
+                </tbody>
+@endforeach               
+              </table>
+<div>
+    @if(sizeof($SiteVisit)>0)
+    {{$SiteVisit->links("pagination::bootstrap-4")}}
+    @endif
+</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+ </div>    
+<div class="container">
+  <div class="card bg-light mt-3">
+      <br>
+   <a style="margin-left:30px;" href="{{asset('storage/csv-formats/site-visit.csv')}}"><button class="btn btn-info">Download CSV Format</button></a>       
+    <div class="card-header">
+      Import and Export SiteVisit list
+    </div>
+    <div class="card-body">
+      <form action="{{route('user.home.SiteVisit.import')}}"
+        method="POST"
+        enctype="multipart/form-data">
+        @csrf
+        <input type="file" name="file"
+          class="form-control" style="width:40%" accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" required>
+        <br>
+        <button class="btn btn-success">
+          Import SiteVisit Record
+        </button>
+        <a class="btn btn-warning"
+        href="{{route('user.home.SiteVisit.export')}}">
+            Export SiteVisit Record
+        </a>
+      </form>
+    </div>
+  </div>
+</div>  
+
+
+ <!--Modal -->
+<div class="modal fade" id="newEventModal22"  tabindex="-1" role="dialog">
+    <form action="{{route('user.home.SiteVisit.SiteVisitNextFollowUp')}}" method="POST">
+        @csrf
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Set New FollowUp Date</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+<div class="modal-body form-horizontal">
+<div class="container-fluid" style="margin-top:30px;">
+<div class="row">
+<div class="col-12">
+<label for="clientgroupname">Stage Name</label><br>    
+<input type="hidden" value="" id="sitevisitids" name="sitevisitids">
+<select class="form-control" id="stages" name="stages" required>
+    <option value="">select stage</option>
+@foreach($SiteVisitStage as $SiteVisitStages)
+
+<option value="{{$SiteVisitStages->stage_name}}">{{$SiteVisitStages->stage_name}}</option>
+@endforeach
+</select>
+</div> 
+
+<div class="col-12">
+<label for="clientgroupname">Followup Time</label><br>    
+<input class="form-control" type="datetime-local" id="followuptime" name="followuptime" required>
+</div> 
+</div>    
+</div>
+
+</div>
+        <div class="modal-footer">
+          <button type="submit" class="btn btn-primary" >Save changes</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+    </div>
+</div>
+</form>
+</div>
+ <!--Modal Ended -->
+
+
+ <!--Modal -->
+<div class="modal fade" id="newEventModal11"  tabindex="-1" role="dialog">
+    <form action="{{route('user.home.SiteVisit.SiteVisitStageCompletion')}}" method="POST">
+        @csrf
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Update Current Stage Status</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+<div class="modal-body form-horizontal">
+<div class="container-fluid" style="margin-top:30px;">
+<div class="row">
+<div class="col-12">
+<label for="clientgroupname">Stage Name</label><br>    
+<input type="hidden" value="" id="sitevisitids2" name="sitevisitids">
+<input type="hidden" value="" id="the_user_iss" name="the_user_iss">
+<input type="text" class="form-control" id="stages2" readonly>
+</div> 
+
+<div class="col-12">
+<label for="clientgroupname">New Status</label><br>    
+<select class="form-control" id="statuscompleted" name="statuscompleted" required>
+     <option value="">Set New Status</option>
+    <option value="1">Completed</option>
+     <option value="2">Hold</option>
+     <option value="0">Pending</option>
+</select>
+</div> 
+</div>    
+</div>
+
+</div>
+        <div class="modal-footer">
+          <button type="submit" class="btn btn-primary" >Update Stage Status</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+    </div>
+</div>
+</form>
+</div>
+ <!--Modal Ended -->
+
+
+<script>
+function SetValueToModal(sitevisitids,stages,datetimes,user_idd)
+{
+$("#stages").val(""+stages);
+$("#sitevisitids").val(""+sitevisitids);
+$("#followuptime").val(""+datetimes.substring(0,16));
+}
+function SetValueToModal11(sitevisitids,stages,datetimes,user_idd)
+{
+$("#stages2").val(""+stages);
+$("#sitevisitids2").val(""+sitevisitids);
+$("#the_user_iss").val(""+user_idd);
+}
+function SearchOnKeyUpp(thiss)
+{
+       
+        $.ajax({
+        url:"{{ route('user.home.SiteVisit.SiteVisitSearch')}}",
+        type: 'POST',
+        headers: {
+         'X-CSRF-TOKEN': $('input[name="_token"]').val()
+        },
+        data:{search:thiss.value},
+        async: false,
+        success: function (searchresult) {
+            if(searchresult.status==false)
+            {
+                 console.log(searchresult);
+            }
+            else
+            {
+                  $("#sitevisitsethtmltothisid").html(""+searchresult);
+            }
+
+        }
+     
+}); 
+    
+}
+</script>
+</x-user.header-without-chart>
