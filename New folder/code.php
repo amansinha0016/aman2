6323941a0f45a450a8cb4e5f7f59479ed4b6f135
amansinha0016 <amansinha0016.com>
2025-04-15 15:33:47 +0530

    php22

--- New folder/code.php
+++ New folder/code.php
@@ -1,1 +1,25 @@
-hellohjohjiodsf
+<?php
+
+function shortAnArray($array) {
+    $shortenedArray = array_map(function($item) {
+        return mb_substr($item, 0, 3); // Shorten each item to the first 3 characters
+    }, $array);
+    return $shortenedArray;
+}
+
+function shortAnArrayWithKeys($array) {
+    $shortenedArray = array_map(function($item) {
+        return mb_substr($item, 0, 3); // Shorten each item to the first 3 characters
+    }, $array);
+    return array_combine(array_keys($array), $shortenedArray); // Keep the original keys
+}
+
+// Example usage:
+$array = ['apple', 'banana', 'cherry'];
+$shortenedArray = shortAnArray($array);
+$shortenedArrayWithKeys = shortAnArrayWithKeys($array);
+
+print_r($shortenedArray);
+print_r($shortenedArrayWithKeys);
+
+?>
